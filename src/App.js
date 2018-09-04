e84e1b4e5283e8fc6847c4d006f869b0c16335ea
justinhurst <justin@grandex.co>
2018-09-04 16:22:18 -0500

    move methods working

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ class App extends Component {
   state = {
     inputText : '',
     list : [
-      {id: 0, text: 'Add some more tasks to this list', time:'Mon May 28 2018 16:12:25 GMT-0500 (CDT)' }
+      {id: 1000, text: 'Add a task to this list', time:'Mon May 28 2018 16:12:25 GMT-0500 (CDT)' }
     ],
     completed : [
-      {id: 0, text: 'Trash completed tasks', time:'Mon May 28 2018 16:12:25 GMT-0500 (CDT)' }
+      {id: 35060, text: 'Remove completed tasks', time:'Mon May 28 2018 16:12:25 GMT-0500 (CDT)' }
     ]
   }
   handleChange = this.handleChange.bind(this);
@@ -21,7 +21,7 @@ class App extends Component {
     if(this.state.inputText){
       const newTask = {
         text: this.state.inputText,
-        id: this.state.list.length + 1,
+        id: Math.floor(Math.random() * 9999999),
         time: new Date()
       }
       const updatedList = this.state.list;
@@ -30,7 +30,8 @@ class App extends Component {
     }
   }
   renderTasks(){
-    return this.state.list.map((task, index) => {
+    if( this.state.list.length > 0){
+      return this.state.list.map((task, index) => {
         return (
             <Task text={task.text}
                 key={index}
@@ -38,42 +39,97 @@ class App extends Component {
                 time={task.time}
                 icon="fas fa-check"
                 markComplete={(id) => this.markTaskComplete(id)}
+                moveTaskUp={(id) => this.moveTaskUp('list',id)}
+                moveTaskDown={(id) => this.moveTaskDown('list',id)}
+                deleteTask={(id) => this.deleteTask('list',id)}
             />
         )
-    })
+      })
+    } else {
+      return <li className="default">You have <i>absolutely</i> nothing to do. Add some tasks to this list</li>
+    }
   }
   renderCompleteTasks(){
-    return this.state.completed.map((task, index) => {
+    if( this.state.completed.length > 0 ){
+      return this.state.completed.map((task, index) => {
         return (
             <Task text={task.text}
                 key={index}
                 id={task.id}
                 time={task.time}
-                icon="fas fa-trash"
+                icon="far fa-trash-alt"
                 markComplete={(id) => this.deleteComplete(id)}
+                moveTaskUp={(id) => this.moveTaskUp('completed',id)}
+                moveTaskDown={(id) => this.moveTaskDown('completed',id)}
+                deleteTask={(id) => this.deleteTask('completed',id)}
             />
         )
-    })
-  }
-  markTaskComplete(id){
-    const updatedList = this.state.list;
-    const updatedCompletedList = this.state.completed;
-    for(let x = 0; x < updatedList.length; x++){
-      if(updatedList[x].id == id){
-        const completedToDo = updatedList.splice(x, 1);
-        updatedCompletedList.push(completedToDo[0]);
-        this.setState({ list : updatedList, completed : updatedCompletedList });
-      }
+      })
+    } else {
+      return <li className="default">There's nothing here...</li>
     }
   }
-  deleteComplete(id){
-    console.log('delete complete');
+  markTaskComplete(id){
     const updatedCompletedList = this.state.completed;
-    for(let x = 0; x < updatedCompletedList.length; x++){
-      if(updatedCompletedList[x].id == id){
-        const completedToDo = updatedCompletedList.splice(x, 1);
-        this.setState({ completed : updatedCompletedList });
+    const taskMarkedComplete = this.state.list.filter((task,index)=>{
+      if(task.id == id) {
+        updatedCompletedList.push(task);
       }
+      return task.id !== id
+    })
+    this.setState({list: taskMarkedComplete, completed: updatedCompletedList});
+  }
+  deleteComplete(id){
+    console.log(id);
+    const deletedTask = this.state.completed.filter((task,index)=>{
+      return task.id !== id
+    })
+    this.setState({completed: deletedTask});
+  }
+  deleteTask(whichList,id){
+    console.log(whichList,id);
+    const deletedTask = this.state[whichList].filter((task,index)=>{
+      return task.id !== id
+    })
+    if(whichList == 'completed'){
+      this.setState({completed: deletedTask});
+    } else {
+      this.setState({list: deletedTask});
+    }
+    
+  }
+  moveTaskUp(whichList,id){
+    //need to combine moveTaskUp and moveTaskDown DRY
+    const movedTask = this.state[whichList];
+    let index = undefined;
+    for(let x = 0; x < movedTask.length; x++){
+      if(movedTask[x].id == id){
+        index = x;
+      }
+    }
+    const removed = movedTask.splice(index,1);
+    movedTask.splice(index-1,0,removed[0])
+    if(whichList == 'completed'){
+      this.setState({completed: movedTask});
+    } else {
+      this.setState({list: movedTask});
+    }
+  }
+  moveTaskDown(whichList,id){
+    //need to combine moveTaskUp and moveTaskDown DRY
+    const movedTask = this.state[whichList];
+    let index = undefined;
+    for(let x = 0; x < movedTask.length; x++){
+      if(movedTask[x].id == id){
+        index = x;
+      }
+    }
+    const removed = movedTask.splice(index,1);
+    movedTask.splice(index+1,0,removed[0])
+    if(whichList == 'completed'){
+      this.setState({completed: movedTask});
+    } else {
+      this.setState({list: movedTask});
     }
   }
   render() {
@@ -81,19 +137,19 @@ class App extends Component {
       <div className="App">
         <header>
           <div>
-          <h2><i className="far fa-list-alt"></i> ToDo List <small>Made with React</small></h2>
+          <h2><i className="far fa-list-alt"></i> TASKLIST <small>Made with React</small></h2>
           </div>
         </header>
         <div id="container">
-          <div id="add-to-list" className="chunk">
+          <div id="add-to-list" className="">
             <form onSubmit={ (e)=> this.addTask(e)}>
               <input type="text" placeholder="What do you need to do?" value={this.state.inputText} onChange={this.handleChange}/>
-              <input type="submit" value="Add ToDo" />
+              <input type="submit" value="Add Task" />
             </form>
           </div>
           <div className="chunk">
             <h3>Things To Do</h3>
-            <ul>{ this.renderTasks() }</ul>
+            <ul id="todo-list">{ this.renderTasks() }</ul>
           </div>
           <div id="completed-list" className="chunk">
             <h3>Completed Tasks</h3>
--- src/Task.js
+++ src/Task.js
@@ -4,11 +4,16 @@ import './App.css';
 class Task extends Component {
     render() {
         return (
-            <li>
-              <div className="item-utilites">
+            <li className="cf">
+              <div className="item-action">
                 <button onClick={ ()=> this.props.markComplete(this.props.id) }><i className={this.props.icon}></i></button>
               </div>
-              { this.props.text }
+              <span>{ this.props.text }</span>
+              <div className="item-utilities">
+              <button onClick={ ()=> this.props.moveTaskUp(this.props.id) }><i className="fas fa-arrow-up"></i></button>
+              <button onClick={ ()=> this.props.deleteTask(this.props.id) }><i className="far fa-trash-alt"></i></button>
+              <button onClick={ ()=> this.props.moveTaskDown(this.props.id) }><i className="fas fa-arrow-down"></i></button>
+              </div>
             </li>
         );
     }
